6503448f1ec1514e3964ea0f735b54ae4333a6e2
Another <89122911+hexvel@users.noreply.github.com>
2023-02-18 11:52:19 +0300

    Add files via upload

---
+++ Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+        <div className="HeroContainer">
+            <h1>
+                This is simply a smarter way to make your legal online
+            </h1>
+        </div>
+    )
+}
+
+export default Hero;
