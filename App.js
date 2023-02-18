103948d45cd13a7deafae4e1648165fd5c637ad8
Another <89122911+hexvel@users.noreply.github.com>
2023-02-18 12:12:39 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Containers/Header.jsx';
+import Hero from './Containers/Hero.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
